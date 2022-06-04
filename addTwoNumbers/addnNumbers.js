5738db9aef75bf31328c4a8f2c133c43d5e2c98f
Lasha <50033103+MistbornL@users.noreply.github.com>
2022-06-04 23:31:25 +0400

    first

--- addTwoNumbers/addnNumbers.js
+++ addTwoNumbers/addnNumbers.js
@@ -1,7 +1,17 @@
 var addTwoNumbers = function (l1, l2) {
-  const mergedList = [...l1.reverse(), ...l2.reverse()].toString();
-  console.log(mergedList);
-  console.log(mergedList.reduce((prev, cur) => prev + cur, 0));
+  const reversed1 = l1.reverse();
+  const reversed2 = l2.reverse();
+  let final1 = "";
+  let final2 = "";
+  for (const each of reversed1) {
+    final1 += each;
+  }
+  for (const each of reversed2) {
+    final2 += each;
+  }
+  const answer = parseInt(final1) + parseInt(final2);
+  console.log([...answer.toString()].reverse().join(""));
+  // console.log(mergedList.reduce((prev, cur) => prev + cur, 0));
 };
 
 const l1 = [2, 4, 3];
