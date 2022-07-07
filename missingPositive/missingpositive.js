98691f4381014e199a34d534e82184d5899b0ac8
Lasha <50033103+MistbornL@users.noreply.github.com>
2022-07-07 16:15:00 +0400

    positive

---
+++ missingPositive/missingpositive.js
@@ -0,0 +1,13 @@
+var i = 1;
+const nums = [1, 2, 0];
+
+nums = nums.sort(function (a, b) {
+  return a - b;
+});
+let n = 1;
+for (let i = 0; n < nums.length; n++) {
+  if (nums[i] === n) {
+    n++;
+  }
+}
+console.log(n);
