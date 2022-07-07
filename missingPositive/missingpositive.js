554bbb917c4edfde1f2a769d1b275d279d8ab3d4
Lasha <50033103+MistbornL@users.noreply.github.com>
2022-07-07 19:59:55 +0400

    positive

--- missingPositive/missingpositive.js
+++ missingPositive/missingpositive.js
@@ -1,13 +1,14 @@
-var i = 1;
-
-const nums = [1, 2, 0];
-nums = nums.sort(function (a, b) {
-  return a - b;
-});
-let n = 1;
-for (let i = 0; n < nums.length; n++) {
-  if (nums[i] === n) {
-    n++;
+function firstPositive(nums) {
+  nums = nums.sort(function (a, b) {
+    return a - b;
+  });
+  var n = 1;
+  for (let i = 0; n < nums.length; n++) {
+    if (nums[i] === n) {
+      n++;
+    }
   }
+  return n;
 }
-console.log(n);
+
+console.log(firstPositive([1, 2, 0]));
