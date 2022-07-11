28e284b27beaada761cf875a358fd542c74fe9dd
Lasha <50033103+MistbornL@users.noreply.github.com>
2022-07-11 13:49:57 +0400

    not done

--- generateParentheses/Parentheses.js
+++ generateParentheses/Parentheses.js
@@ -1,14 +1,28 @@
 const parentheses = (n) => {
   const solution = [];
-  const generate = (left, right, partial) => {
-    if (left > right) return;
-    if (!left && !right) solution.push(partial);
+  const stack = [];
+  const generate = (left, right) => {
+    if ((left === right) === n) {
+      solution.push(stack.join(""));
+      return;
+    }
 
-    if (left > 0) generate(left - 1, right, partial + "(");
-    if (left > 0) generate(left, right - 1, partial + ")");
+    if (left < n) {
+      stack.append("(");
+      generate(left + 1, right);
+      stack.pop();
+    }
+    if (right < n) {
+      stack.append("(");
+      generate(left, right + 1);
+      stack.pop();
+    }
   };
 
-  generate(n, n, "");
+  generate(n, n);
   return solution;
 };
 console.log(parentheses(3));
+
+const arr = ["a", "b"];
+console.log(arr.join(""));
