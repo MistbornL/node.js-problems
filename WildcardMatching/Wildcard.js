1d4dde111e2334270e2962846d7f9362205d38fa
MistbornL <lashaloria3@gmail.com>
2022-07-13 20:23:44 +0400

    wildcard

--- WildcardMatching/Wildcard.js
+++ WildcardMatching/Wildcard.js
@@ -10,8 +10,18 @@ function wildcard(s, p) {
   for (let i = 0; i < sArray.length; i++) {
     if (sArray[i] === pArray[i]) {
       return true;
+    } else if (pArray[i] === "?") {
+      return true;
+    } else {
+      return false;
     }
   }
+  // var merged = sArray.concat(pArray);
+  // for (let i = 0; i < sArray.length; i++) {
+  //   for (let b = sArray.length - 1; b < sArray.length; b++) {
+  //     console.log(merged[i], merged[b]);
+  //   }
+  // }
 }
 
-console.log(wildcard("as", "as"));
+console.log(wildcard("asd", "?cd"));
--- WildcardMatching/tempCodeRunnerFile.js
+++ WildcardMatching/tempCodeRunnerFile.js
@@ -1,14 +1,1 @@
-function wildcard(s, p) {
-  var sArray = s.split("");
-  var pArray = p.split("");
   console.log(sArray);
-
-  if (pArray.includes("*")) {
-    return true;
-  }
-
-  var merged = sArray.concat(pArray);
-  console.log(merged);
-}
-
-console.log(wildcard("as", "df"));
